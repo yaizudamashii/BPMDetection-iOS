36dca18943493ecd925da51a61e741e00ae5c5cb
Yuki Konda <yukikonda@iheartmedia.com>
2016-01-29 11:40:42 -0500

    Initial Commit

---
+++ BPMDetect/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BPMDetect
+//
+//  Created by Yuki Konda on 1/29/16.
+//  Copyright © 2016 Yuki Konda. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ BPMDetect/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BPMDetect
+//
+//  Created by Yuki Konda on 1/29/16.
+//  Copyright © 2016 Yuki Konda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
